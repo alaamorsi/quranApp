46990bfd9309a0fb8e32449a746042b69d037ebe
Alaa Morsy <alaamorsi@gamil.com>
2023-08-29 19:55:02 +0300

    initial commit

---
+++ lib/quran_screen.dart
@@ -0,0 +1,345 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors
+
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
+
+class QuranScreen extends StatefulWidget {
+  String name;
+
+  late String nameQ;
+
+  int index;
+
+  late int indexQ;
+
+  QuranScreen({required this.name, required this.index, super.key}) {
+    nameQ = name;
+    indexQ = index;
+  }
+
+  List<String> quranList = [
+    '001',
+    '002',
+    '003',
+    '004',
+    '005',
+    '006',
+    '007',
+    '008',
+    '009',
+    '010',
+    '011',
+    '012',
+    '013',
+    '014',
+    '015',
+    '016',
+    '017',
+    '018',
+    '019',
+    '020',
+    '021',
+    '022',
+    '023',
+    '024',
+    '025',
+    '026',
+    '027',
+    '028',
+    '029',
+    '030',
+    '031',
+    '032',
+    '033',
+    '034',
+    '035',
+    '036',
+    '037',
+    '038',
+    '039',
+    '040',
+    '041',
+    '042',
+    '043',
+    '044',
+    '045',
+    '046',
+    '047',
+    '048',
+    '049',
+    '050',
+    '051',
+    '052',
+    '053',
+    '054',
+    '055',
+    '056',
+    '057',
+    '058',
+    '059',
+    '060',
+    '061',
+    '062',
+    '063',
+    '064',
+    '065',
+    '066',
+    '067',
+    '068',
+    '069',
+    '070',
+    '071',
+    '072',
+    '073',
+    '074',
+    '075',
+    '076',
+    '077',
+    '078',
+    '079',
+    '080',
+    '081',
+    '082',
+    '083',
+    '084',
+    '085',
+    '086',
+    '087',
+    '088',
+    '089',
+    '090',
+    '091',
+    '092',
+    '093',
+    '094',
+    '095',
+    '096',
+    '097',
+    '098',
+    '099',
+    '100',
+    '101',
+    '102',
+    '103',
+    '104',
+    '105',
+    '106',
+    '107',
+    '108',
+    '109',
+    '110',
+    '111',
+    '112',
+    '113',
+    '114',
+  ];
+
+  @override
+  State<QuranScreen> createState() => _QuranScreenState();
+}
+
+class _QuranScreenState extends State<QuranScreen> {
+  bool playing = false;
+  IconData playButton = Icons.play_arrow;
+  late AudioPlayer player;
+
+  Stream<PositionData> get positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+        player.positionStream,
+        player.bufferedPositionStream,
+        player.durationStream,
+        (position, bufferedPosition, duration) => PositionData(
+          position,
+          bufferedPosition,
+          duration ?? Duration.zero,
+        ),
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    player = AudioPlayer()..setAsset('assets/quran/${widget.quranList[widget.indexQ]}.mp3');
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey[800]!,
+              Colors.grey[400]!,
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 48.0,
+          ),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(
+                    '${widget.nameQ}',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Text(
+                    'القارئ الشيخ عبدالباسط عبدالصمد',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                Center(
+                  child: Container(
+                    width: 280,
+                    height: 280,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/1_foreground.png')),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 18.0,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30.0),
+                        topLeft: Radius.circular(30.0),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 500.0,
+                        ),
+                        StreamBuilder<PositionData>(
+                            stream: positionDataStream,
+                            builder: (context, snapshot) {
+                              final positionData = snapshot.data;
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                child: ProgressBar(
+                                  barHeight: 8.0,
+                                  baseBarColor: Colors.white,
+                                  bufferedBarColor: Colors.grey,
+                                  progressBarColor: Colors.blue,
+                                  thumbColor: Colors.blue,
+                                  timeLabelTextStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  progress:
+                                      positionData?.position ?? Duration.zero,
+                                  buffered: positionData?.bufferedPosition ??
+                                      Duration.zero,
+                                  total:
+                                      positionData?.duration ?? Duration.zero,
+                                  onSeek: player.seek,
+                                ),
+                              );
+                            }),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Row(
+                          children: [
+                            Controls(player: player),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PositionData {
+  const PositionData(
+    this.position,
+    this.bufferedPosition,
+    this.duration,
+  );
+
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+}
+
+class Controls extends StatelessWidget {
+  const Controls({super.key, required this.player});
+
+  final AudioPlayer player;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+        stream: player.playerStateStream,
+        builder: (context, snapshot) {
+          final playerState = snapshot.data;
+          final processingState = playerState?.processingState;
+          final playing = playerState?.playing;
+          if (!(playing ?? false))
+            return IconButton(
+              onPressed: player.play,
+              icon: Icon(Icons.play_arrow_rounded),
+              iconSize: 80.0,
+              color: Colors.white,
+            );
+          else if (processingState != ProcessingState.completed){
+            return IconButton(
+              onPressed: player.pause,
+              icon: Icon(Icons.pause_rounded),
+              iconSize: 80.0,
+              color: Colors.white,
+            );
+          }
+          return Icon((Icons.play_arrow_rounded),
+            size: 80.0,
+            color: Colors.white,
+          );
+        });
+  }
+}
