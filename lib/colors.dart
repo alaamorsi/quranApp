1f3dae42291be0acaae34998bc99c9e34f7d0f84
alaamorsi <alaamorsi20022020@gmail.com>
2023-08-30 18:22:46 +0300

    test.2

--- lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-var backGroundColor = Colors.brown.shade400;
-var appBarColor =  Colors.brown.shade900;
-var soraButtonColor =  Colors.black54;
--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,66 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/colors.dart';
 import 'package:quran_app/quran_list.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backGroundColor,
       appBar: AppBar(
         title: Text(
           'القرآن الكريم',
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28.0),
         ),
         centerTitle: true,
-        backgroundColor: appBarColor,
+        backgroundColor: Colors.brown.shade900,
         elevation: 0.0,
       ),
-      body: Column(
-        children: [
-          //for the image
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0,vertical: 20.0),
-            child: Container(height: 400,width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/firstScreenImage.png')
-                      ,fit: BoxFit.cover),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.brown[200]!,
+              Colors.brown,
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            //for the image
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0,vertical: 20.0),
+              child: Container(height: 400,width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/firstScreenImage.png')
+                        ,fit: BoxFit.cover),
+                  ),
+              ),
+            ),
+            //for the first button
+            InkWell(
+              onTap: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => QuranListView()));
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Container(
+                  height: 90.0,
+                  width: 300.0,
+                  decoration: BoxDecoration(
+                    color: Colors.brown[800],
+                    borderRadius: BorderRadius.circular(25.0),),
+                  child: Center(child: Text('استمع للقرآن',style: TextStyle(color: Colors.white,fontSize: 22.0),)),
                 ),
+              ),
             ),
-          ),
-          //for the first button
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: Container(
-              height: 90.0,
-              width: 300.0,
-              decoration: BoxDecoration(
-                color: soraButtonColor,
-                borderRadius: BorderRadius.circular(25.0),),
-              child: MaterialButton(
-                onPressed: (){Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => QuranListView()));},
-                child: Text('استمع للقرآن',style: TextStyle(color: Colors.white,fontSize: 22.0),),),
+            //for the second button
+            InkWell(
+              onTap: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => QuranListView()));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: 90.0,
+                  width: 300.0,
+                  decoration: BoxDecoration(
+                    color: Colors.brown[800],
+                    borderRadius: BorderRadius.circular(25.0),),
+                  child: Center(child: Text('قراءة القرآن',style: TextStyle(color: Colors.white,fontSize: 22.0),)),
+                ),
+              ),
             ),
-          ),
-          //for the second button
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              height: 90.0,
-              width: 300.0,
-              decoration: BoxDecoration(
-                color: soraButtonColor,
-                borderRadius: BorderRadius.circular(25.0),),
-              child: MaterialButton(
-                onPressed: (){Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => QuranListView()));},
-                child: Text('قراءة القرآن',style: TextStyle(color: Colors.white,fontSize: 22.0),),),
-            ),
-          ),
-        ]
+          ]
+        ),
       ),
     );
   }
