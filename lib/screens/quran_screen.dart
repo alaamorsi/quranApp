bc15ff0dbd82abba5650b14455cfe66a3929fb36
CADCATSamehAhmed <samehahmedfawzy777next@gmail.com>
2023-09-09 20:13:32 +0300

    final edite

--- lib/screens/quran_screen.dart
+++ lib/screens/quran_screen.dart
@@ -70,8 +70,8 @@ class _QuranScreenState extends State<QuranScreen> {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              Colors.green.shade300,
-              Colors.green
+              Colors.blueGrey.shade300,
+              Colors.blueGrey.shade600
             ],
           ),
         ),
