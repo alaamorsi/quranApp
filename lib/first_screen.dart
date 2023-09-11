d853be99dd2fee3a945b7c7e99122b7a801a6e2f
alaamorsi <alaamorsi20022020@gmail.com>
2023-09-11 22:06:12 +0300

    test18

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quran_app/screens/el_quraa.dart';
 import 'package:quran_app/screens/motshabhat.dart';
 import 'package:quran_app/screens/mushaf.dart';
+import 'package:quran_app/screens/questions.dart';
 import 'screens/al_azkaar.dart';
 import 'screens/masbaha.dart';
 import 'our_widgets.dart';
@@ -39,7 +40,7 @@ class _FirstScreenState extends State<FirstScreen> {
               firstScreenItem(context: context, title: 'المصحف', navTo: MushafScreen(), icon: Icons.import_contacts),
               ]
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(height: 10.0,),
             Row(
               children: [
                 firstScreenItem(context: context, title: 'المتشابهات', navTo: MotshabhatScreen(), icon:Icons.ballot),
@@ -47,7 +48,7 @@ class _FirstScreenState extends State<FirstScreen> {
                 firstScreenItem(context: context, title: 'المسبحة', navTo: Masbaha(), icon:Icons.data_saver_on),
               ],
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(height: 10.0,),
             Row(
               children: [
                 firstScreenItem(context: context, title: 'أذكار الصباح', navTo: MorAndEvenAzkaar(time: 'أذكار الصباح'), icon: Icons.light_mode),
@@ -55,7 +56,15 @@ class _FirstScreenState extends State<FirstScreen> {
                 firstScreenItem(context: context, title: 'أذكار المساء', navTo: MorAndEvenAzkaar(time: 'أذكار المساء'), icon: Icons.dark_mode),
               ]
               ),
-            ]
+              SizedBox(height: 10.0,),
+            Row(
+              children: [
+                firstScreenItem(context: context, title: 'راجع حفظك', navTo: Questions(), icon: Icons.book_rounded),
+                SizedBox(width: 20.0,),
+                firstScreenItem(context: context, title: 'تعلم الصلاة', navTo: MorAndEvenAzkaar(time: 'أذكار المساء'), icon: Icons.man_sharp),
+              ]
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -7,6 +7,20 @@ class Questions extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar(context: context, title: 'راجع حفظك'),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blueGrey.shade300,
+              Colors.blueGrey.shade600
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
